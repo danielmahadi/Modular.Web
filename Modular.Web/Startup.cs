dbe76ef307434c960584e4397dfbad845b41159d
Daniel Mahadi <daniel.mahadi@telepathy.ai>
2019-06-10 16:31:16 +0800

    Adds modular.web

---
+++ Modular.Web/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ExtCore.WebApplication.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Modular.Web
+{
+    public class Startup
+    {
+        private IHostingEnvironment _env;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _env = hostingEnvironment ?? throw new ArgumentNullException(nameof(hostingEnvironment));
+        }
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var extensionPath = Path.Combine(_env.ContentRootPath, "Extensions");
+            services.AddExtCore(extensionPath);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseExtCore();
+        }
+    }
+}
